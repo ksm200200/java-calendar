552c0a82bd36d212492cf1c26ee9174f3544cf7b
user <user@DESKTOP-L2KK7E8>
2021-07-28 18:55:39 +0900

    test

--- src/sm/calendar/Calendar.java
+++ src/sm/calendar/Calendar.java
@@ -5,7 +5,14 @@ import java.util.Scanner;
 public class Calendar {
 
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
+	public boolean isLeapYear(int year) {
+		if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+			return true;
+			
+		return false;
+	}
 	// 달의 최대 날짜를 돌려주는 함수
 	public int getMaxDaysOfMonth(int month) {
 		return MAX_DAYS[month - 1];
@@ -33,11 +40,6 @@ public class Calendar {
 			}
 		}
 		System.out.println();
-//		System.out.println(" 1   2   3    4   5   6   7");
-//		System.out.println(" 8   9  10   11  12  13  14");
-//		System.out.println("15  16  17   18  19  20  21");
-//		System.out.println("22  23  24   25  26  27  28 ");
-//		System.out.println("-------------------------------");
 	}
 
 	//main
